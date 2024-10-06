2b50c906b43eea0560a1aa2c30023634763f5ad0
amarigom <andreamarigomez@gmail.com>
2024-10-06 19:47:53 -0300

    modificacion 1 desde mi repolocal

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld{
 	public static void main(String[]args){
 		System.out.println("***********Hello world & welcome tu Jenkins+*******");
+                System.out.println("***********Agregando linea uno desde mi laptop block de notas+*******");
 		}
 }
