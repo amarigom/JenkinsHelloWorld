706b604ba97f7cc1dff634c5d13cdc83196fe95a
amarigom <andreamarigomez@gmail.com>
2024-10-07 11:13:33 -0300

    Probando webhook modificando desde laptop

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld{
 	public static void main(String[]args){
 		System.out.println("***********Hello world & welcome tu Jenkins+*******");
                 System.out.println("***********Agregando linea uno desde mi laptop block de notas+*******");
+                System.out.println("***********Agregando linea dos desde mi laptop block de notas+ probando WEBhook*******");
 		}
 }
